e75fc62bd5cc56a4066b1663c09c50ef7ba240c1
Anurag9977 <anuragchoudhury852@gmail.com>
2019-08-26 20:57:55 +0530

    Updates

---
+++ precache-manifest.25b5f5c5e5cc609889787572b18687a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafac41eab94a4da6340197cec10c158",
+    "url": "/TODOLIST/index.html"
+  },
+  {
+    "revision": "05223db9065b2eb21648",
+    "url": "/TODOLIST/static/css/main.04ef53fb.chunk.css"
+  },
+  {
+    "revision": "e16ced53e11e669427ae",
+    "url": "/TODOLIST/static/js/2.a1fbc514.chunk.js"
+  },
+  {
+    "revision": "05223db9065b2eb21648",
+    "url": "/TODOLIST/static/js/main.ff660ef4.chunk.js"
+  },
+  {
+    "revision": "72b0dc64a2e8045659d6",
+    "url": "/TODOLIST/static/js/runtime~main.1f3b7400.js"
+  },
+  {
+    "revision": "4b37a2c08fd4289222c75fd022a1e4c4",
+    "url": "/TODOLIST/static/media/background.4b37a2c0.jpg"
+  }
+]);
